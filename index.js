6cd38d263e7447bdbdb4ffd61eafbeb0df399470
rina <tes>
2025-05-01 12:01:07 +0800

    Inital commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+//Middleware
+
+app.use(express.json());
+
+//Endpoint List
+app.get('/', (req, res) => {
+    res.send('Respon dari API Node.js!');
+});
+
+app.get('/users', (req, res) => {
+    const users = [
+        { id: 1, name: 'Rina'},
+        { id: 2, name: 'Lina'}
+    ];
+    res.send(users);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server berjalan di http://localhost:${PORT}`);
+});
+
